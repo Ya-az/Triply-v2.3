3713dba133dcd8e57a4160e0ee6b7d1ee30a6e3e
Ya-az <yazeedalharbe8@gmail.com>
2025-11-12 18:24:58 +0300

    🔧 Fix: Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Triply-main/',
+  base: '/Triply-v2.3/',
   server: {
     host: true,
     port: 5173
